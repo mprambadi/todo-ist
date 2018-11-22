0b0e7fd7314d48c5e0f0de189209a9e2e858c206
Maul <mprambadi@gmail.com>
2018-11-22 17:53:50 +0700

    Updates

---
+++ precache-manifest.f728f58a57535050730aaea9dc395c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81d2eeb40850c89ab463",
+    "url": "/todo-ist/static/js/runtime~main.81d2eeb4.js"
+  },
+  {
+    "revision": "6c8ea2adb32da304e4b7",
+    "url": "/todo-ist/static/js/main.6c8ea2ad.chunk.js"
+  },
+  {
+    "revision": "dba4e91717e9f4eb8a21",
+    "url": "/todo-ist/static/js/1.dba4e917.chunk.js"
+  },
+  {
+    "revision": "6c8ea2adb32da304e4b7",
+    "url": "/todo-ist/static/css/main.f2f484c2.chunk.css"
+  },
+  {
+    "revision": "dba4e91717e9f4eb8a21",
+    "url": "/todo-ist/static/css/1.7b402438.chunk.css"
+  },
+  {
+    "revision": "6cf7535d682e7a9c9d1b29b41b914830",
+    "url": "/todo-ist/index.html"
+  }
+];
