c766faff59096e01fe083a9615e8486c7940c736
Maul <mprambadi@gmail.com>
2018-11-22 17:52:38 +0700

    deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
+import { users } from "./users";
 
 class App extends Component {
+  constructor(props){
+      super(props)
+
+      this.state = {
+        users,
+        filter: "",
+        items: [],
+        coba: ""
+      };
+
+      this.kekirim = this.kekirim.bind(this)
+
+  }
+
+  componentDidMount() {
+    Notification.requestPermission().then(function(result) {
+      console.log(result);
+    });
+  }
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+  spawnNotification(body, icon, title) {
+    const options = {
+      body: body,
+      icon: icon
+    };
+    new Notification(title, options);
+  }
+
+  kekirim (e) {
+    e.preventDefault();
+
+    this.setState(state => {
+      return {
+        users: [
+          ...state.users,
+          {
+            id: Date.now(),
+            name: state.coba,
+            username: state.coba,
+            email: state.coba
+          }
+        ]
+      };
+    });
+  };
+
+
   render() {
+    const userFilter = this.state.users.filter(
+      user =>
+        [user.name, user.email, user.username]
+          .join(" ")
+          .toLowerCase()
+          .indexOf(this.state.filter) !== -1
+    );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Editz <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="d-flex justify-content-center">
+        <div className="d-flex flex-column w-50 mt-5">
+          <input
+            name="filter"
+            value={this.state.filter}
+            onChange={this.handleChange}
+            className="form-control mb-2"
+          />
+
+          {userFilter.map(user => (
+            <div className="App-content-item" key={user.id}>
+              <div>{user.username}</div>
+              <div style={{ backgroundColor: this.state.filter === "a" ? "none" : "" }}>
+                {user.name}
+              </div>
+              <div>{user.email}</div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
